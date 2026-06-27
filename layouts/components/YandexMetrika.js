15fc725cfec052887bb479e5c4e7d6952124a32b
Mark <7rodma@gmail.com>
2026-06-27 18:39:27 +0700

    Prevent duplicate Yandex Metrika script injection

--- layouts/components/YandexMetrika.js
+++ layouts/components/YandexMetrika.js
@@ -8,17 +8,23 @@ const YandexMetrika = ({ counterId }) => {
   return (
     <Script id="yandex-metrika" strategy="afterInteractive">
       {`
-        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){
-          (m[i].a=m[i].a||[]).push(arguments)};
+        (function(m,e,t,r,i,k,a){
+          m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
           m[i].l=1*new Date();
+          for (var j = 0; j < document.scripts.length; j++) {
+            if (document.scripts[j].src === r) { return; }
+          }
           k=e.createElement(t);a=e.getElementsByTagName(t)[0];
           k.async=1;k.src=r;a.parentNode.insertBefore(k,a)
         })(window,document,"script",
-           "https://mc.yandex.ru/metrika/tag.js","ym");
+           "https://mc.yandex.ru/metrika/tag.js?id=${counterId}","ym");
 
         ym(${counterId},"init",{
+          ssr: true,
           webvisor: true,
           clickmap: true,
+          referrer: document.referrer,
+          url: location.href,
           accurateTrackBounce: true,
           trackLinks: true
         });
